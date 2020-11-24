ef2f287bb8dc8aad60244fcfd85b8466e4c62508
vg2712 <vivek9i28r@gmail.com>
2020-11-24 21:11:22 +0530

    removed env

--- src/environment/index.js
+++
@@ -1,9 +0,0 @@
-import {DevEnvironment} from './dev.env';
-import {ProdEnvironment} from './prod.env';
-export function getEnvVariable(){
-    if (__DEV__) {
-      return DevEnvironment;
-    } else {
-      return ProdEnvironment;
-    }
-}
